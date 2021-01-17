b174148acc1c9f436927933623a60f28d561d234
Caleb Schoepp <caleb.schoepp@gmail.com>
2021-01-17 02:01:02 -0700

    Proper escaping

--- src/hoverWidget.ts
+++ src/hoverWidget.ts
@@ -2,8 +2,8 @@ import { MarkdownString } from 'vscode';
 import { getDocItem } from './docs';
 
 export const hoverWidgetContent = (title: String, linkText: String, link: String, description: String, code: String) => {
-    const content = new MarkdownString(`# ${title} \n ## [${linkText}](${link}) \n ${description}\n\`${code}\``);
-
+    const content = new MarkdownString(`# ${title} \n ## [${linkText}](${link}) \n ${description} \n \n *Labelled Code:* \`\`${code}\`\``);
+    console.log(content.value);
     return {
         contents: [content],
     };
