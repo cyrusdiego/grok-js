2bb13c22702f0c6c3f690e524322dae46bf93a9e
Armianto Sumitro <sumitro@ualberta.ca>
2021-01-16 22:27:16 -0700

    Format inlineDecorator

--- src/inlineDecoratorType.ts
+++ src/inlineDecoratorType.ts
@@ -3,7 +3,7 @@ import * as vscode from 'vscode';
 const inlineDecoratorType = vscode.window.createTextEditorDecorationType({
     after: {
         margin: '0 0 0 3em',
-        color: new vscode.ThemeColor('editorWhitespace.foreground')
+        color: new vscode.ThemeColor('editorWhitespace.foreground'),
     },
 });
 
