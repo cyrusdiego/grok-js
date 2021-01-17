e2fabcece4384aafee543857b046b0995101ea67
drojasca <drojasca@ualberta.ca>
2021-01-16 18:42:06 -0700

    brken

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,63 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+import * as path from 'path';
+import { workspace, ExtensionContext, window } from 'vscode';
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "grok-js" is now active!');
+import { LanguageClient, LanguageClientOptions, ServerOptions, TransportKind } from 'vscode-languageclient/node';
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('grok-js.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+let client: LanguageClient;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from grok-js!');
+export function activate(context: ExtensionContext) {
+	const outputChannel = window.createOutputChannel('grokJs');
+	let a = 20;
+
+	window.onDidChangeTextEditorSelection((e) => {
+		const document = e.textEditor.document.getText();
+		outputChannel.appendLine("hello");
 	});
+	
+	//TODO: remove
 
-	context.subscriptions.push(disposable);
+    // The server is implemented in node
+    let serverModule = context.asAbsolutePath(path.join('server', 'out', 'server.js'));
+    // The debug options for the server
+    // --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
+    let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+
+    // If the extension is launched in debug mode then the debug server options are used
+    // Otherwise the run options are used
+    let serverOptions: ServerOptions = {
+        run: { module: serverModule, transport: TransportKind.ipc },
+        debug: {
+            module: serverModule,
+            transport: TransportKind.ipc,
+            options: debugOptions,
+        },
+    };
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for plain text documents
+        documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+        synchronize: {
+            // Notify the server about file changes to '.clientrc files contained in the workspace
+            fileEvents: workspace.createFileSystemWatcher('**/.clientrc'),
+        },
+    };
+
+    // Create the language client and start the client.
+    client = new LanguageClient('grokJS', 'grokJS', serverOptions, clientOptions);
+
+    // Start the client. This will also launch the server
+    client.start();
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate(): Thenable<void> | undefined {
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
+}
