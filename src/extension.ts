fdcaf751e91277cc53c351b77a2cc043dd0d6c76
Cyrus Diego <cyrus.d04@gmail.com>
2021-01-16 19:12:55 -0700

    formatting for widget

--- src/extension.ts
+++ src/extension.ts
@@ -1,28 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-import { languages } from 'vscode';
+import {
+  languages,
+  TextDocument,
+  Position,
+  ExtensionContext,
+  CancellationToken,
+  MarkdownString,
+} from 'vscode';
+
+const hoverWidget = (title: String, link: String, description: String) => {
+  return `# ${title} \n ## [${link}](https://developer.mozilla.org/en-US/) \n ${description}`;
+};
 
 // this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  vscode.commands.registerCommand('grok-js.grokJS', () => {});
-
-  let disposable = languages.registerHoverProvider('javascript', {
-    provideHover(document, position, token) {
+export function activate(context: ExtensionContext) {
+  const hoverRegistration = languages.registerHoverProvider('javascript', {
+    provideHover(
+      document: TextDocument,
+      position: Position,
+      token: CancellationToken
+    ) {
       const range = document.getWordRangeAtPosition(position);
       const word = document.getText(range);
-
+      const title = 'Object instantiation';
+      const documentation_link = 'Working with Objects';
+      const documentation_blob = `JavaScript is designed on a simple object-based paradigm. 
+                                An object is a collection of properties, and a property is an association between a name (or key)
+                                and a value. A property's value can be a function, in which case the property is known as a method.
+                                In addition to objects that are predefined in the browser, you can define your own objects.
+                                This chapter describes how to use objects, properties, functions, and methods, and how to
+                                create your own objects.`;
       return {
-        contents: [word],
+        contents: [
+          new MarkdownString(
+            hoverWidget(title, documentation_link, documentation_blob)
+          ),
+        ],
       };
     },
   });
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(hoverRegistration);
 }
 
 // this method is called when your extension is deactivated
