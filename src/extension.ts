19478b9ae3292302c4555ca286f34f11bc7963d9
Cyrus Diego <cyrus.d04@gmail.com>
2021-01-16 21:09:41 -0700

    example hover box

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import { languages, TextDocument, Position, ExtensionContext, CancellationToken, MarkdownString } from 'vscode';
-
-const hoverWidget = (title: String, link: String, description: String) => {
-    return `# ${title} \n ## [${link}](https://developer.mozilla.org/en-US/) \n ${description}`;
-};
+import { hoverWidgetContent } from './hoverWidget';
 
 // this method is called when your extension is activated
 export function activate(context: ExtensionContext) {
@@ -13,16 +10,15 @@ export function activate(context: ExtensionContext) {
             const range = document.getWordRangeAtPosition(position);
             const word = document.getText(range);
             const title = 'Object instantiation';
-            const documentation_link = 'Working with Objects';
-            const documentation_blob = `JavaScript is designed on a simple object-based paradigm. 
+            const linkText = 'Working with Objects';
+            const link = 'https://github.com/cyrusdiego/grok-js/tree/text-decorator';
+            const blob = `JavaScript is designed on a simple object-based paradigm. 
                                 An object is a collection of properties, and a property is an association between a name (or key)
                                 and a value. A property's value can be a function, in which case the property is known as a method.
                                 In addition to objects that are predefined in the browser, you can define your own objects.
                                 This chapter describes how to use objects, properties, functions, and methods, and how to
                                 create your own objects.`;
-            return {
-                contents: [new MarkdownString(hoverWidget(title, documentation_link, documentation_blob))],
-            };
+            return hoverWidgetContent(title, linkText, link, blob);
         },
     });
 
